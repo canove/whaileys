8a3496c820bd8882d30525850eaa2e1455ff1dc9
canove <cassio@economicros.com.br>
2025-12-19 18:00:31 -0300

    wip: send reporting tokens

--- src/Socket/messages-send.ts
+++ src/Socket/messages-send.ts
@@ -35,6 +35,10 @@ import {
 } from "../Utils";
 import { getUrlInfo } from "../Utils/link-preview";
 import {
+  getMessageReportingToken,
+  shouldIncludeReportingToken
+} from "../Utils/reporting-utils";
+import {
   areJidsSameUser,
   BinaryNode,
   BinaryNodeAttributes,
@@ -745,6 +749,33 @@ export const makeMessagesSocket = (config: SocketConfig) => {
         });
 
         logger.debug({ jid }, `adding biz node for buttons message`);
+      }
+
+      if (
+        shouldIncludeReportingToken(message) &&
+        message.messageContextInfo?.messageSecret // TODO always send reporting?
+      ) {
+        const reportingKey: WAMessageKey = {
+          id: msgId,
+          fromMe: true,
+          remoteJid: destinationJid,
+          participant: participant?.jid
+        };
+
+        const reportingNode = await getMessageReportingToken(
+          encodedMsg,
+          message,
+          reportingKey
+        ).catch(err => {
+          logger.warn({ jid, err }, "failed to attach reporting token");
+
+          return null;
+        });
+
+        if (reportingNode) {
+          (stanza.content as BinaryNode[]).push(reportingNode);
+          logger.info({ jid }, "added reporting token to message");
+        }
       }
 
       logger.debug(
